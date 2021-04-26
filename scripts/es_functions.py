d56db19e4382c181c616873634ddbb3ff4a071c5
Ben Elsworth <be15516@bristol.ac.uk>
2021-04-26 14:35:31 +0100

    testing params

--- scripts/es_functions.py
+++ scripts/es_functions.py
@@ -22,7 +22,7 @@ es = Elasticsearch(
 )
 
 TITLE_WEIGHT = 1
-ABSTRACT_WEIGHT = 1
+ABSTRACT_WEIGHT = 0.95
 
 def vector_query(
     index_name:str, query_vector:list, record_size:int=100000, search_size:int=100, score_min:int=0, year_range:list=[1950,2021]
@@ -60,8 +60,8 @@ def vector_query(
                 "query": script_query,
                 "_source": {"includes": ["doc_id", "year", "sent_num", "sent_text"]},
                 "indices_boost": [
-                    {"title_sentence_vectors": TITLE_WEIGHT},
-                    {"abstract_sentence_vectors": ABSTRACT_WEIGHT},
+                    {"use_title_sentence_vectors": TITLE_WEIGHT},
+                    {"use_abstract_sentence_vectors": ABSTRACT_WEIGHT},
                 ],
             },
         )
@@ -166,8 +166,8 @@ def standard_query(index_name:str, text:str, year_range:list=[1950,2021]):
         },
         "_source": ["doc_id", "sent_num", "sent_text", "year"],
         "indices_boost": [
-            {"title_sentence_vectors": TITLE_WEIGHT},
-            {"abstract_sentence_vectors": ABSTRACT_WEIGHT},
+            {"use_title_sentence_vectors": TITLE_WEIGHT},
+            {"use_abstract_sentence_vectors": ABSTRACT_WEIGHT},
         ],
     }
     res = es.search(
@@ -197,32 +197,34 @@ def combine_full_and_vector(index_name:str, query_text:str, query_vector:list, r
         "query": {
             "bool": {
                 "should": [
-                    #{
-                    #    "match": {
-                    #        "sent_text": {
-                    #            "query": query_text     
-                    #        }
-                    #    }
-                    #},
+                    {
+                        "match": {
+                            "sent_text": {
+                                "query": query_text,     
+                                #"boost" : 10
+                            },                            
+                        },
+                         
+                    },
                     {
                         "script_score": {
                             "query" : {"match_all" : {}},
                             "script":{
                                 # +1 to deal with negative results (script score function must not produce negative scores)
-                                "source": "10 * cosineSimilarity(params.query_vector, 'sent_vector') +1",
+                                "source": "50 * cosineSimilarity(params.query_vector, 'sent_vector') +1",
                                 "params": {"query_vector": query_vector},
                             },
-                            "boost" : 100
+                            #"boost" : 100
                         },
                     },        
                 ],
             }
         },
         "_source": ["doc_id", "sent_num", "sent_text", "year"],
-        #"indices_boost": [
-        #    {"title_sentence_vectors": TITLE_WEIGHT},
-        #    {"abstract_sentence_vectors": ABSTRACT_WEIGHT},
-        #],
+        "indices_boost": [
+            {"use_title_sentence_vectors": TITLE_WEIGHT},
+            {"use_abstract_sentence_vectors": ABSTRACT_WEIGHT},
+        ],
     }
     res = es.search(
         ignore_unavailable=True, request_timeout=TIMEOUT, index=index_name, body=body
--- search-test.py
+++ search-test.py
@@ -1,4 +1,4 @@
-from scripts.es_functions import combine_full_and_vector
+from scripts.es_functions import combine_full_and_vector, standard_query, vector_query
 from scripts.general import load_spacy_model
 from loguru import logger
 import pandas as pd
@@ -7,13 +7,16 @@ import os
 
 
 query = 'genome wide association studies'
+query = 'Systematic reviews of genetic association'
+query = 'machine learning'
 vector_index_name = "use_*_sentence_vectors"
 
-f = 'gwas-vec.npy'
+f = query.replace(' ','_')+'.npy'
 if os.path.exists(f):
     logger.info(f'{f} done')
     test_vec = np.load(f)
 else:
+    logger.info('Running NLP')
     nlp = load_spacy_model()
     doc = nlp(query)
     test_vec = doc.vector
@@ -22,6 +25,27 @@ else:
 
 logger.info(len(test_vec))
 
-res = combine_full_and_vector(index_name = vector_index_name, query_text=query, query_vector=test_vec)
-for r in res["hits"]["hits"][0:5]:
-    logger.info(r)
+top = 10
+res1 = combine_full_and_vector(index_name = vector_index_name, query_text=query, query_vector=test_vec)
+d = []
+for r in res1["hits"]["hits"][0:top]:
+    rr = r['_source']
+    rr['index'] = r['_index']
+    rr['score'] = r['_score']
+    d.append(rr)
+df1 = pd.DataFrame(d)
+logger.info(f'COMBINED\n{df1[["index","sent_text","score"]]}')
+
+res2 = standard_query(index_name=vector_index_name,text=query) 
+d = []
+for r in res2["hits"]["hits"][0:top]:
+    rr = r['_source']
+    rr['score'] = r['_score']
+    rr['index'] = r['_index']
+    d.append(rr)
+df2 = pd.DataFrame(d)
+logger.info(f'FULL TEXT\n{df2[["index","sent_text","score"]]}')
+
+res3 = vector_query(index_name=vector_index_name,query_vector=test_vec)
+df3 = pd.DataFrame(res3[0:top])
+logger.info(f'VECTOR\n{df3[["index","sent_text","score"]]}')
